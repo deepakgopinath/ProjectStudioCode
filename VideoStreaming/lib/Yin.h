3747b247a224626d64c1452c8f17be55839c7e0f
hank5925 <hank5925@gmail.com>
2014-03-12 21:42:58 -0400

    Add Pitch Detection Library

---
+++ VideoStreaming/lib/Yin.h
@@ -0,0 +1,32 @@
+/* The code Yin.h and Yin.cpp was referenced from Joren Six.
+ * See more detail about the implementation please check https://github.com/JorenSix/Pidato
+ * Ying-Shu Kuo
+ * */
+
+#ifndef Yin_h
+#define Yin_h
+class Yin{
+	public: 
+		Yin();
+		~Yin();
+		Yin(float sampleRate,int bufferSize);
+		void initialize(float sampleRate,int bufferSize);
+
+		float getPitch(float* buffer);
+		float getProbability();
+
+	private: 
+		float parabolicInterpolation(int tauEstimate);
+		int absoluteThreshold();
+		void cumulativeMeanNormalizedDifference();
+		void difference(float* buffer);
+		
+		double threshold;
+		int bufferSize;
+		int halfBufferSize;
+		float sampleRate;
+		float* yinBuffer;
+		float probability;
+};
+
+#endif
